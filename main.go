d3150de782f367613a0df4b1f5064fbfb97e9c0a
Clayton King <ClaytonKing03@gmail.com>
2024-04-20 20:49:20 -0400

    CRUD operations done

--- main.go
+++ main.go
@@ -50,21 +50,24 @@ func main() {
 		MaxAge:           300,
 	}))
 
-	server := &http.Server{
-		Handler: router,
-		Addr:    ":" + portString,
-	}
-
 	v1Router := chi.NewRouter()
 	v1Router.Get("/healthz", handlerReadiness)
 	v1Router.Get("/err", handlerErr)
 	v1Router.Post("/users", apiCfg.handlerCreateUser)
 	v1Router.Get("/users", apiCfg.middlewareAuth(apiCfg.handlerGetUser))
 
-	v1Router.Post("/feeds", apiCfg.middlewareAuth(apiCfg.handlerCreateFeed))
 	v1Router.Get("/feeds", apiCfg.handlerGetFeeds)
 
+	v1Router.Post("/feed_follows", apiCfg.middlewareAuth(apiCfg.handlerCreateFeedFollow))
+	v1Router.Get("/feed_follows", apiCfg.middlewareAuth(apiCfg.handlerGetFeedFollows))
+	v1Router.Delete("/feed_follows/{feedFollowID}", apiCfg.middlewareAuth(apiCfg.handlerDeleteFeedFollow))
+
 	router.Mount("/v1", v1Router)
+
+	server := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
 
 	log.Printf("Server starting on port %v...", portString)
 	err = server.ListenAndServe()
